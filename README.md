cabb3aeaab641394b0f1f5b4cb667ac5978a7dfb
x200706 <99391710+x200706@users.noreply.github.com>
2023-09-26 14:58:21 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+### Require
+- Python3
+- Pandas2.1.1
+```shell
+pip install pandas==2.1.0
+```
+- A account `*.csv` file which is named `acc.csv`, format：
+```
+名稱, 日期, 收支, 分類, 金額
+資產盤點, 20230921, I, 資產, 15000
+眼科檢查, 20230925, O, 醫療保健, 100
+雜貨店, 20230925, O, 飲食, 199
+義麵館, 20230925, O, 飲食, 315
+寶雅, 20230926, O, 衣物或衛生, 700
+```
+>**PS：**
+>
+>'I' means 'input' and 'O' means 'output'.
---
+++ main.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import os
+
+# def check():
+#   pwd = os.getenv("MIMA")
+#   user_input_pwd = input('使用本程式前，先核對身分：')
+#   if user_input_pwd != pwd:
+#     print('你不是本人...請回吧> <|||')
+#     exit()
+
+# check()
+
+# 載入記帳
+file_path = 'acc.csv'
+df = pd.read_csv(file_path)
+df.columns = df.columns.str.replace(' ', '')  # trim
+
+# 顯示記帳
+print(df)
+
+# ---------------情報區 --------------- #
+df['金額'] = pd.to_numeric(df['金額']) # 字串轉貨幣
+
+# 計算總收入
+total_income = df[df['收支'] == ' I']['金額'].sum()
+print('總收入：', total_income)
+
+# 計算總支出
+total_expense = df[df['收支'] == ' O']['金額'].sum()
+print('總支出：', total_expense)
+
+# 計算總資產
+print('總資產：', total_income - total_expense)
